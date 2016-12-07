c6802d30d864ab6c9cd0e80f77a7263da3642e3c
Sukhmeet Singh <sukhmeet032795@gmail.com>
2016-12-08 02:37:21 +0530

    Code Changes: Addition of image url link

--- MovieTrailerWebsite/movieProject/entertainment_center.py
+++ MovieTrailerWebsite/movieProject/entertainment_center.py
@@ -1,1 +1,22 @@
 import media
+
+toystory = media.Movie(
+		
+		"ToyStory", 
+		"https://www.youtube.com/watch?v=4KPTXpQehio", 
+		"Imagination runs rampant when toys become mobile when not watched. Two toys, Woody and Buzz Lightyear despise each other like no other. But, when the toys are separated from their home, a truce is formed between them all in an effort to journey home.", 
+		['The adventure takes off!', 'Guts of steel', 'Buzz off!', 'Oooh...3-D'], 
+		"http://www.imdb.com/title/tt0114709/mediaviewer/rm3813007616"
+	)
+
+avatar = media.Movie(
+
+		"Avatar",
+		"https://www.youtube.com/watch?v=cRdxXPV9GNQ",
+		"A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+		['Return to Pandora', 'Enter the World', 'Worth watching', 'Eye-popping spectacle of conflict'],
+		"http://www.imdb.com/title/tt0499549/mediaviewer/rm843615744"
+	)
+
+print (toystory.story)
+print (avatar.story)
--- MovieTrailerWebsite/movieProject/media.py
+++ MovieTrailerWebsite/movieProject/media.py
@@ -6,14 +6,16 @@ class Movie(object):
 	url = ''
 	story = ''
 	reviews = []
+	poster = ''
 
-	def __init__(self, title, youtubeUrl, storyLine, reviews):
+	def __init__(self, movie_title, movie_url, movie_storyLine, movie_reviews, movie_posterUrl):
 
-		self.title = title
-		self.url = youtubeUrl
-		self.story = storyLine
+		self.title = movie_title
+		self.url = movie_url
+		self.story = movie_storyLine
+		self.poster = movie_posterUrl
 
-		for review in reviews:
+		for review in movie_reviews:
 			self.reviews.append(review)
 
 	def showTitle(self):
@@ -24,8 +26,12 @@ class Movie(object):
 
 	def showReviews(self):
 		return self.reviews
+
+	def showPoster(self):
+		webbrowser.open(self.poster)	
+		return self.poster
 		
 	def showTrailer(self):
 		webbrowser.open(self.url)
-		return self.url					
+		return self.url							
 
