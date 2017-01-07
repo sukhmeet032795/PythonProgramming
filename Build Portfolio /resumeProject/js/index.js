40b638517f89aa9ea14e7fe59bf79cfb91c64fbb
Sukhmeet Singh <sukhmeet032795@gmail.com>
2017-01-07 13:46:24 +0530

    resume project update

---
+++ Build Portfolio /resumeProject/js/index.js
@@ -0,0 +1,40 @@
+$(function(){
+
+    var index = 1, mar = 0;
+    var childCount = $('.rightPanel').children().length;
+    var parentWidth = parseInt($('.rightPanel').css("width"));
+
+    $('.rightPanel').on('click', '#next', function(){
+
+        var current = $(this);
+        var present = $("#current");
+
+        var width = parseInt($(present).css("width"));
+
+        mar += Math.ceil((100*width)/parentWidth);
+
+        $(present).removeAttr("id");
+        $(current).removeAttr("id");
+        $(current).attr("id", "current");
+
+        $('.rightPanel').css("transform", "translateX(-" + mar + "%)");
+
+        if(index != childCount){
+
+            index++;
+            var nextEl = $('.rightPanel .box:nth-child('+(index+1)+')');
+            $(nextEl).attr("id", "next");
+        }
+        else{
+
+            var presentEl = $('.rightPanel .box:nth-child(1)');
+            $(presentEl).attr("id", "current");
+            var nextEl = $('.rightPanel .box:nth-child(2)');
+            $(nextEl).attr("id", "next");
+            index = 1;
+        }
+    });
+
+
+});
+
