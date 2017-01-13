ba721e96d7b8979750847f8c80d4ecd62414b5fa
Sukhmeet Singh <sukhmeet032795@gmail.com>
2017-01-14 00:00:57 +0530

    Templates

---
+++ MultiUserProject/Templates/index.py
@@ -0,0 +1,31 @@
+import webapp2
+import cgi
+import jinja2
+import os
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.headers['Content Type'] = "text/html"
+        self.response.out.write(*a, **kw)
+
+    def render_str(self,template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        a = self.render_str(template,**kw)
+        self.write(a)
+
+class MainPage(Handler):
+    def get(self):
+        items = self.request.get_all("food")
+        self.render("shoppingList.html", items = items)
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                              ])
+
+
