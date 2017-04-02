2bf3885cf66fea81cd0cfafdbf768ec70c5d52b3
Sukhmeet Singh <sukhmeet032795@gmail.com>
2017-04-03 00:32:46 +0530

    Rename folder to CatClickers

--- Neighbourhood Map/Udacity_AJAX_INIT/CatClickers/script.js
+++ Neighbourhood Map/Udacity_AJAX_INIT/CatClickers/script.js
@@ -51,4 +51,15 @@ $(function(){
         cats[id].clicks += 1;
         $(el.childNodes[1]).html(cats[id].clicks + " Clicks");
     });
+
+    // The application should display
+    //     a list of at least 5 cats, listed by name
+    //     an area to display the selected cat
+
+    // In the cat display area, the following should be displayed
+    //     the cat's name
+    //     a picture of the cat
+    //     text showing the number of clicks
+
+
 })
